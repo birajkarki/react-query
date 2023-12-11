fe52cd4d934c2cc8f8f6d61f636826429aa5c501
birajkarki <birajkarki9849@gmail.com>
2023-12-11 14:42:17 +0545

    learning react-query

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.jsx";
+import Products from "./Products.jsx";
+import Product from "./Product.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/products/:productId",
+    element: <Product />,
+  },
+]);
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
